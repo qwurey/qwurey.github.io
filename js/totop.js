d719fdb9ad6ab0bf419b4fa93d27445216cd5ccc
urey.qw <urey.qw@qq.com>
2016-03-05 16:01:44 +0800

    Site updated: 2016-03-05 16:01:44

--- js/totop.js
+++ js/totop.js
@@ -1,12 +1,29 @@
-$(window).scroll(function() {
-    $(window).scrollTop() > 500 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
-});
-$("#rocket").click(function() {
-    $("#rocket").addClass("launch");
-    $("html, body").animate({
-        scrollTop: 0
-    }, 500, function() {
-        $("#rocket").removeClass("show launch");
+(function($) {
+    
+    // When to show the scroll link
+    // higher number = scroll link appears further down the page    
+    var upperLimit = 1000; 
+        
+    // Our scroll link element
+    var scrollElem = $('#totop');
+    
+    // Scroll to top speed
+    var scrollSpeed = 500;
+    
+    // Show and hide the scroll to top link based on scroll position    
+    scrollElem.hide();
+    $(window).scroll(function () {             
+        var scrollTop = $(document).scrollTop();        
+        if ( scrollTop > upperLimit ) {
+            $(scrollElem).stop().fadeTo(300, 1); // fade back in            
+        }else{        
+            $(scrollElem).stop().fadeTo(300, 0); // fade out
+        }
     });
-    return false;
-});
+
+    // Scroll to top animation on click
+    $(scrollElem).click(function(){ 
+        $('html, body').animate({scrollTop:0}, scrollSpeed); return false; 
+    });
+
+})(jQuery);
